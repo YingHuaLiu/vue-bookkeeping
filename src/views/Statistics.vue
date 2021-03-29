1449f6669a50d26d1ac16aa4af84f8fe97d64354
YingHuaLiu <1042413535@qq.com>
2021-03-29 08:39:52 +0800

    finish basic version

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -20,27 +20,27 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-import Tabs from '@/components/Tabs.vue';
-import recordTypeList from '@/constants/recordTypeList';
-import clone from '@/lib/clone';
-import dayjs from 'dayjs';
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
+import Tabs from '@/components/Tabs.vue'
+import recordTypeList from '@/constants/recordTypeList'
+import clone from '@/lib/clone'
+import dayjs from 'dayjs'
 
 @Component({
   components: {Tabs}
 })
 export default class Statistics extends Vue {
   get recordList() {
-    return (this.$store.state as RootState).recordList;
+    return (this.$store.state as RootState).recordList
   }
 
   get groupedList() {
-    const {recordList} = this;
-    if (recordList.length === 0) {return [] as Result;}
+    const {recordList} = this
+    if (recordList.length === 0) {return [] as Result}
     const newList = clone(recordList)
         .filter(r => r.type === this.type)
-        .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+        .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
 
     type  Result = {
       title: string;
@@ -48,34 +48,34 @@ export default class Statistics extends Vue {
       items: RecordItem[];
     }[];
 
-    const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+    const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}]
     for (let i = 1; i < newList.length; i++) {
-      const current = newList[i];
-      const last = result[result.length - 1];
+      const current = newList[i]
+      const last = result[result.length - 1]
       if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
-        last.items.push(current);
+        last.items.push(current)
       } else {
-        result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+        result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]})
       }
     }
-    result.map(group => {group.total = group.items.reduce((sum, item) => sum + item.amount, 0);});
-    return result;
+    result.map(group => {group.total = group.items.reduce((sum, item) => sum + item.amount, 0)})
+    return result
   }
 
-  type = '-';
-  interval = 'day';
-  recordTypeList = recordTypeList;
+  type = '-'
+  interval = 'day'
+  recordTypeList = recordTypeList
 
   beforeCreate() {
-    this.$store.commit('fetchRecords');
+    this.$store.commit('fetchRecords')
   }
 
   tagString(tags: Tag[]) {
-    return tags.length === 0 ? '无' : tags.join(',');
+    return tags.length === 0 ? '无' : tags.join(',')
   }
 
   beautify(date: string) {
-    return date;
+    return date
   }
 }
 </script>
